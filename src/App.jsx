15f39621f70cbbbb82e5ffcb18698bf490fa036a
kumaresan2001 <kumaresancrseven@gmail.com>
2023-02-21 19:47:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,27 @@ import EditForm from "./pages/EditForm";
 import AddForm from "./pages/AddForm";
 import Dashboard from "./pages/Dashboard";
 import "bootstrap/dist/css/bootstrap.css";
+import Paper from "@mui/material/Paper";
 
 export const url = "https://63ef4f91c59531ccf16ca737.mockapi.io/Products";
 
 function App() {
+  const bgstyles = {
+    borderRadius: "0px",
+    minHeight: "100vh",
+    backgroundColor: "pink",
+  };
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add" element={<AddForm />} />
-        <Route path="/edit/:id" element={<EditForm />} />
-      </Routes>
+      <Paper sx={bgstyles} elevation={4}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add" element={<AddForm />} />
+          <Route path="/edit/:id" element={<EditForm />} />
+        </Routes>
+      </Paper>
     </div>
   );
 }
